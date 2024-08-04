430704761127c324a764f345b98b6269bf150c1c
alanis <alanislima0110@gmail.com>
2024-08-04 13:54:21 -0300

    DobroETercaParte

--- Exercicio007/src/Calculo.java
+++ Exercicio007/src/Calculo.java
@@ -1,13 +1,8 @@
 import java.util.Scanner;
 
 public class Calculo {
-    //7) Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a
-    //sua terça parte.
-    //Ex:
-    //Digite um número: 3.5
-    //O dobro de 3.5 é 7.0
-    //A terça parte de 3.5 é 1.16666
     Scanner leituraTeclado = new Scanner(System.in);
+    
     public void funcao(){
         System.out.println("Digite um numero real: ");
         double numeroDigitado = leituraTeclado.nextDouble();
